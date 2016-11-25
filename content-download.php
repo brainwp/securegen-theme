31229b98fc8d78fb00b62d65d2853d59303f1c98
Diego Rojas <diego@brasa.art.br>
2016-11-25 19:58:55 -0200

    finalizada funcionalidade do template de download obtido via shortcode e resolvido content de page em mobile

--- content-download.php
+++ content-download.php
@@ -1,14 +1,16 @@
 <div class="col-sm-3 download-info">
-<?php if ( $dlm_download->has_version_number() ) {
-	printf( __( 'Version %s', 'download-monitor' ), $dlm_download->get_the_version_number() );
-} ?>
+	<?php if ( $dlm_download->has_version_number() ) {
+		printf( __( 'Version %s', 'download-monitor' ), $dlm_download->get_the_version_number() );
+	} ?>
 </div>
 <div class="col-sm-3 download-info">
-<?php $dlm_download->the_title(); ?>
+	<?php $dlm_download->the_title(); 
+	?>
 </div>
 <div class="col-sm-3 download-info">
-<?php the_date('M/Y'); ?>
+	<?php echo date_i18n( get_option( 'date_format' ), strtotime( $dlm_download->get_the_file_date() )); 
+	?>
 </div>
-<a class="col-sm-3 download-button" href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow"><i class="fa fa-download" aria-hidden="true"></i> Download
-</a>
+	<a class="col-sm-3 download-button" href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow"><i class="fa fa-download" aria-hidden="true"></i> Download
+	</a>
 <br />
